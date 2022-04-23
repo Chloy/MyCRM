1091c04ae1d736b1adfea38b79432c22a6f0aba2
Hella <krisbeaky@gmail.com>
2022-04-23 23:16:40 +0500

    Start to use form for creation instead of trying manualy define sign up fields

--- crm_app/views.py
+++ crm_app/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
 class SignUp(CreateView):
     template_name = 'registration/signup.html'
-    model = User
+    form_class = UserCreationForm
     success_url = reverse_lazy('crm_app:login')
-    fields = ['username', 'password', 'email', 'is_staff']
 
 
 class Home(ListView):
